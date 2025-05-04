720f30e9ca85495f34cba20290bb279fc322a81e
matchamatcho <pyomyokur0@gmail.com>
2025-05-04 20:40:36 +0900

    17です

--- 16.cpp
+++ 16.cpp
@@ -31,7 +31,25 @@ const ll INFL = 4e18;
 int main()
 {
     //aaa
-    
+    int n;
+    cin>>n;
+    vint p(n),q(n);
+    rep(i,n){
+        cin>>p[i];
+
+    }
+    rep(i,n)cin>>q[i];
+    vint r(n);
+    rep(i,n)r[i]=i+1;
+    int a=0,b=0,i=0;
+    do{
+        if(p==r)a=i;
+        if(q==r)b=i;
+        i++;
+        
+
+    }while(next_permutation(r.begin(), r.end()));
+    cout<<abs(a-b)<<endl;
 
     return 0;
 }
