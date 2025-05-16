50d13bc5d40d14e8eae8da21e154cfb29d8bec8f
matchamatcho <pyomyokur0@gmail.com>
2025-05-16 11:51:57 +0900

    状態数少なくてもbitdp

--- 51.cpp
+++ 51.cpp
@@ -30,7 +30,40 @@ const ll INFL = 4e18;
 
 int main()
 {
-    
+    int N;
+    cin>>N;
+    string S;
+    cin>>S;
+    vint dp((1<<3));
+    dp[0];//011が参加して、jが鍵を持って帰る。
+    map<int ,char>mp;
+    mp[0]='J';mp[1]='O';mp[2]='I';
+    dp[1]=1;
+    rep(i,N){
+        vint old(8);
+        swap(old,dp);
+        int sekinin;
+        if(S[i]=='J')sekinin=0;
+        else if(S[i]=='O')sekinin=1;
+        else sekinin=2;
+        rep(j,8){//今
+            rep(k,8){//前
+                if((j&(1<<sekinin))==0)continue;
+                if((k&j)==0)continue;
+                dp[j]=(dp[j]+old[k])%10007;
+
+            }
+            
+        }
+        // printv(dp);
+      
+    }
+    int ans=0;
+    rep(i,8){
+        ans=(ans+dp[i])%10007;
+    }
+    cout<<ans<<endl;
+
     
 
     return 0;
