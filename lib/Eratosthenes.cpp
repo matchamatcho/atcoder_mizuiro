4c59359fb0803c4ac9b3d5bf7abcff291f7af809
matchamatcho <pyomyokur0@gmail.com>
2025-06-09 23:34:52 +0900

    エラトステネスのふるいををibに追加

---
+++ lib/Eratosthenes.cpp
@@ -0,0 +1,23 @@
+#include<bits/stdc++.h>
+using namespace std;
+//この問題で扱う整数は最大4×10^12であり、int型の上限を超えるため、long longを用いる。
+//実装の簡略化のためlong longをllとして使えるようにする。（以下、llという型はlong long型と同じ）
+using ll = long long;
+//コピペする場合はここから～
+//Era(n)を呼んだ後、isprime[i]=iが素数かどうか　となっている。
+vector < bool > isprime;
+//返り値は素数のリスト。
+vector < ll > Era(int n) {
+	isprime.resize(n, true);
+	vector < ll > res;
+	isprime[0] = false;
+	isprime[1] = false;
+	for(ll i = 2; i < n; ++i) isprime[i] = true;
+	for(ll i = 2; i < n; ++i) {
+		if(isprime[i]) {
+			res.push_back(i);
+			for(ll j = i * 2; j < n; j += i) isprime[j] = false;
+		}
+	}
+	return res;
+}
