4640642c629eab61b5f1a92f460b546af060d936
matchamatcho <pyomyokur0@gmail.com>
2025-09-19 21:54:40 +0900

    98まだ

---
+++ 96.cpp
@@ -0,0 +1,40 @@
+#include <bits/stdc++.h>
+// #include <atcoder/all>
+using namespace std;
+// using namespace atcoder;
+// using mint = modint998244353;
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pll = pair<ll,ll>;
+using vstr = vector<string>;
+using vint = vector<int>;
+using vvi = vector<vint>;
+using vll = vector<ll>;
+using vvll = vector<vll>;
+using vpii = vector<pii>;
+using vvpii = vector<vpii>;
+using vpll = vector<pll>;
+#define mkpr make_pair
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define Rep(i,a,b) for(int i=a;i<b;i++)
+#define ALL(x) (x).begin(),(x).end()
+#define printv(x); for(auto now : x) cout << now << " "; cout << endl;
+#define yes(q) cout << ((q) ? "Yes" : "No") << endl;
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+const int INF =1001001001;
+const ll INFL = 4e18;
+
+
+int main()
+{
+    ll n;
+    cin>>n;
+
+    cout<<n*(n-1)/2<<endl;
+    
+
+    return 0;
+}
