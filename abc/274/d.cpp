8508dc33cd7726206920d773d961507653c5530b
matchamatcho <pyomyokur0@gmail.com>
2026-02-17 13:37:53 +0900

    a

---
+++ abc/274/d.cpp
@@ -0,0 +1,82 @@
+#include <bits/stdc++.h>
+using namespace std;
+#if __has_include(<atcoder/all>)
+#include <atcoder/all>
+using namespace atcoder;
+#endif
+// using mint = modint998244353;
+using ll = long long;
+using ull = unsigned long long;
+using ld = long double;
+using pii = pair<int, int>;
+using pll = pair<ll,ll>;
+using vstr = vector<string>;
+using vint = vector<int>;
+using vvi = vector<vint>;
+using vll = vector<ll>;
+using vvll = vector<vll>;
+using vpii = vector<pii>;
+using vvpii = vector<vpii>;
+using vpll = vector<pll>;
+#define mkpr make_pair
+#define rep(i, n) for (int i = 0; i < (n); ++i)
+#define Rep(i,a,b) for(int i=a;i<b;i++)
+#define ALL(x) (x).begin(),(x).end()
+#define printv(x); for(auto now : x) cout << now << " "; cout << endl;
+#define yes(q) cout << ((q) ? "Yes" : "No") << endl;
+template<class T> inline bool chmax(T& a, T b) { if (a < b) { a = b; return 1; } return 0; }
+template<class T> inline bool chmin(T& a, T b) { if (a > b) { a = b; return 1; } return 0; }
+const int INF =1001001001;
+const ll INFL = 4e18;
+
+
+int main()
+{
+    int n,x,y;
+    cin>>n>>x>>y;
+    vint a(n);
+    rep(i,n)cin>>a[i];
+
+    {
+        vint dp(3e4);
+        dp[a[0]+1e4]=1;
+        rep(i,(n+1)/2-1){
+            vint old(3e4);
+            swap(dp,old);
+            rep(j,3e4){
+                if(old[j]>0){
+                    // cout<<j<<endl;
+                    dp[j+a[2*i+2]]=1;
+                    dp[j-a[2*i+2]]=1;
+                }
+            }
+        }
+        if(dp[x+1e4]==0){
+            cout<<"No"<<endl;
+            return 0;
+        }
+    }
+
+    {
+        vint dp(3e4);
+        dp[1e4]=1;
+        rep(i,n/2){
+            vint old(3e4);
+            swap(dp,old);
+            rep(j,3e4){
+                if(old[j]>0){
+                    dp[j+a[2*i+1]]=1;
+                    dp[j-a[2*i+1]]=1;
+                }
+            }
+        }
+        if(dp[y+1e4]==0){
+            cout<<"No"<<endl;
+            return 0;
+        }
+    }
+    cout<<"Yes"<<endl;
+    
+
+    return 0;
+}
